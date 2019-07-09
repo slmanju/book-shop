e0002dc3affe282850b93b9ccbe59a3790f9d82c
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-07-09 11:40:02 +0530

    create components structure.

--- book-shop-admin/src/app/app.module.ts
+++ book-shop-admin/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './components/app.component';
 
 @NgModule({
   declarations: [
